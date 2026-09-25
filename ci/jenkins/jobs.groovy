d24298ac22e2aa8715041c0dd72a91567d44c6b0
Sam Rivera <sam@example.com>
2026-09-26 01:11:48 +0800

    Add the Jenkins controller as code: plugins, casc.yaml and Job DSL

---
+++ ci/jenkins/jobs.groovy
@@ -0,0 +1,28 @@
+// Job DSL: one multibranch job per BookNest repository
+['booknest-jenkins'].each { repo ->
+  multibranchPipelineJob(repo) {
+    displayName("BookNest (${repo})")
+    branchSources {
+      branchSource {
+        source {
+          github {
+            id(repo)
+            repoOwner('binarybehemoth')
+            repository(repo)
+            repositoryUrl("https://github.com/binarybehemoth/${repo}")
+            configuredByUrl(false)
+            credentialsId('github-booknest')
+            traits {
+              gitHubBranchDiscovery { strategyId(1) }
+              gitHubPullRequestDiscovery { strategyId(1) }
+            }
+          }
+        }
+      }
+    }
+    orphanedItemStrategy {
+      discardOldItems { daysToKeep(7); numToKeep(5) }
+    }
+    triggers { periodicFolderTrigger { interval('4h') } }
+  }
+}
